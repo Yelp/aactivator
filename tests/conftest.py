3fb8480f9856c02259dcd524cc9e7b93f549ed02
Kenny Do <chinesedewey@gmail.com>
2023-02-16 22:23:09 -0800

    add back cwd fixture using os.chdir instead

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,20 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import os
+
 import pytest
+
+
+@pytest.fixture(autouse=True)
+def cwd():
+    old_dir = os.getcwd()
+    os.chdir('/')
+
+    try:
+        yield
+    finally:
+        os.chdir(old_dir)
 
 
 @pytest.fixture
